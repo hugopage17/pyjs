6dada8675f0939980b594e6daac20511d01e8085
Hugo Page <hugopage0017@gmail.com>
2019-12-22 15:28:31 +1300

    15:28/22

--- app.py
+++ app.py
@@ -1,22 +1,13 @@
 import eel
-from get_time import GetTime
 from pythonping import ping
 
 eel.init('web')
 
 @eel.expose
-def runTime(num):
-    try:
-        return int(num)*2
-    except:
-        error = 'Error: invalid entry'
-        return error
-
-@eel.expose
 def ping_ip(ip):
     try:
         response_list = ping(ip, size=32, count=10)
-        return 'average response time: {}ms'.format(response_list.rtt_avg_ms)
+        return response_list.rtt_avg_ms
     except:
         return "Host unreachable"
 
--- web/app.js
+++ web/app.js
@@ -1,10 +1,48 @@
-let array = []
-
-function getTime() {
+let latency = []
+let count = []
+var c = 1
+var config = {
+    type: 'line',
+    data: {
+        labels: count,
+        datasets: [{
+            label: 'Latency ms',
+            data: latency,
+            fill: false,
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+}
+window.onload = function() {
+  var ctx = document.getElementById('myChart').getContext('2d');
+  window.myLine = new Chart(ctx, config);
+}
+function connect() {
+  document.getElementById('first-page').hidden = true
+  document.getElementById('second-page').hidden = false
   setInterval(async function(){
+    count.push(c)
+    c++
     let ip = document.getElementById('name').value
     let value = await eel.ping_ip(ip)();
-    array.push(value)
-    console.log(array);
+    latency.push(value)
+    config.data.datasets[0].data = latency
+    window.myLine.update()
+    let avg = latency.reduce((a, b) => a + b, 0)/latency.length
+    avg = avg.toFixed(2)
+    let time = value.toFixed(2)
+    document.getElementById('current-ping').innerText = `Time = ${time}ms`
+    document.getElementById('latency-avg').innerText = `Average = ${avg}ms`
   },1000)
 }
