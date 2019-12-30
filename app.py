c0076b4b0fae1a2d913f2ffcd65462f1ff2a53e0
Hugo Page <hugopage0017@gmail.com>
2019-12-31 04:22:23 +1300

    31/12-04:22

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import eel
 from pythonping import ping
 import getpass
+import subprocess
 
 eel.init('web')
 
@@ -12,7 +13,7 @@ def get_user():
 @eel.expose
 def ping_ip(ip, bytes, timeout):
     try:
-        response_list = ping(ip, size=int(bytes), timeout=int(timeout), count=10)
+        response_list = ping(ip, size=int(bytes), timeout=int(timeout), count=1)
         return response_list.rtt_avg_ms
     except:
         return "Host unreachable"
@@ -22,5 +23,42 @@ def get_html(file):
     f = open('html-strings/'+file, "r")
     html = f.read()
     return html
+
+@eel.expose
+def wifi_scan():
+    results = subprocess.check_output(["netsh", "wlan", "show", "network", "mode=Bssid"])
+    results = results.decode("ascii")
+    results = results.replace("\r","")
+    ls = results.split("\n")
+    ls = ls[4:]
+    ssids = []
+    x = 0
+    for l in ls:
+        i = l.split(':')
+        sd = i[0][0:4]
+        if sd == 'SSID':
+            name = i[1]
+        elif i[0] == '         Signal             ':
+            strength = i[1]
+        try:
+            obj = {
+                "name":name,
+                "signal":strength
+            }
+            ssids.append(obj)
+        except:
+            pass
+    arr = []
+    for i in range(0, len(ssids)):
+        if ssids[i] not in ssids[i+1:]:
+            arr.append(ssids[i])
+    new_arr = []
+    for h in range(0, len(arr)):
+        try:
+            if arr[h]['name'] is not arr[h-1]['name']:
+                new_arr.append(arr[h])
+        except:
+            pass
+    return new_arr
 
 eel.start('main.html', size=(1240, 860))
--- web/app.js
+++ web/app.js
@@ -26,42 +26,64 @@ var config = {
     }
 }
 
-function connect() {
-  setInterval(async function(){
-    count.push(c)
-    let ip = document.getElementById('name').value
-    let size = document.getElementById('size').value
-    let timeout = document.getElementById('timeout').value
-    let value = await eel.ping_ip(ip, size, timeout)();
-    latency.push(value)
-    config.data.datasets[0].data = latency
-    window.myLine.update()
-    let avg = latency.reduce((a, b) => a + b, 0)/latency.length
-    avg = avg.toFixed(2)
-    let time = value.toFixed(2)
-    var p = document.createElement('DIV')
-    p.id = 'ping-count-single'
-    var p1 = document.createElement('label')
-    p1.innerText = `Bytes: ${size}`
-    p1.id = 'inner-ping-count'
-    var p2 = document.createElement('label')
-    p2.innerText = `Host: ${ip}`
-    p2.id = 'inner-ping-count'
-    p3 = document.createElement('label')
-    p3.innerText = `Timeout: ${timeout}`
-    p3.id = 'inner-ping-count'
-    p4 = document.createElement('label')
-    p4.innerText = `Response: ${value}ms`
-    p4.id = 'inner-ping-count'
-    p.appendChild(p1)
-    p.appendChild(p2)
-    p.appendChild(p4)
-    p.appendChild(p3)
-    document.getElementById('ping-counts').appendChild(p)
-    document.getElementById('latency-avg').innerText = `Average: ${avg}ms`
-    document.getElementById('packets-sent').innerText = `Packets Sent: ${c}`
-    var element = document.getElementById("ping-counts");
-    element.scrollTop = element.scrollHeight;
-    c++
-  },1000)
+let max = 0
+let min = 0
+
+async function pingFunc(){
+  count.push(c)
+  let ip = document.getElementById('name').value
+  let size = document.getElementById('size').value
+  let timeout = document.getElementById('timeout').value
+  let value = await eel.ping_ip(ip, size, timeout)();
+  if(value >= max){
+    max = value
+  }
+  if(min != 0 && min >= value){
+    min = value
+  }
+  else if(min == 0){
+    min = value
+  }
+  latency.push(value)
+  config.data.datasets[0].data = latency
+  window.myLine.update()
+  let avg = latency.reduce((a, b) => a + b, 0)/latency.length
+  avg = avg.toFixed(2)
+  let time = value.toFixed(2)
+  var p = document.createElement('DIV')
+  p.id = 'ping-count-single'
+  var p1 = document.createElement('label')
+  p1.innerText = `Bytes: ${size}`
+  p1.id = 'inner-ping-count'
+  var p2 = document.createElement('label')
+  p2.innerText = `Host: ${ip}`
+  p2.id = 'inner-ping-count'
+  p3 = document.createElement('label')
+  p3.innerText = `Timeout: ${timeout}`
+  p3.id = 'inner-ping-count'
+  p4 = document.createElement('label')
+  p4.innerText = `Response: ${value}ms`
+  p4.id = 'inner-ping-count'
+  p.appendChild(p1)
+  p.appendChild(p2)
+  p.appendChild(p4)
+  p.appendChild(p3)
+  document.getElementById('ping-counts').appendChild(p)
+  document.getElementById('latency-avg').innerText = `Average: ${avg}ms`
+  document.getElementById('min').innerText = `Minimum: ${min}ms`
+  document.getElementById('max').innerText = `Maximum: ${max}ms`
+  document.getElementById('packets-sent').innerText = `Packets Sent: ${c}`
+  var element = document.getElementById("ping-counts");
+  element.scrollTop = element.scrollHeight;
+  c++
+}
+
+let runPing
+
+function startPing() {
+  runPing = setInterval(pingFunc,1000)
+}
+
+function stopPing(){
+  clearInterval(runPing)
 }
